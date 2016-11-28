deefa3461f0b4fceabe86a988ada73b6b8ee97fe
eddievillan <villanuevaedward89@gmail.com>
2016-11-27 18:57:51 -0800

    refactored 'changePizzaSizes(size)' to batch layout changes before implementing them. WAAYY FASTER!

--- js/main.js
+++ js/main.js
@@ -449,12 +449,18 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(randomPizzas[0], size)
+    var newWidth = [];
+
+    randomPizzas.forEach(function(pizza){
+      newWidth.push((pizza.offsetWidth + dx) + 'px');
+    })
+
+    for(var i = 0; i < newWidth.length; i++) {
+      randomPizzas[i].style.width = newWidth[i];
     }
-  }
+}
 
   changePizzaSizes(size);
 
